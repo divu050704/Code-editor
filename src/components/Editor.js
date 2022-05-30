0a91d0d7e9068f53bfd096267e88e06472949c6b
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2022-05-30 10:32:41 +0530

    New languages added
    Updated Editor component for total 6 languages

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,15 +2,36 @@ import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
 import { javascript } from "@codemirror/lang-javascript";
+import {html} from "@codemirror/lang-html"
+import { markdown } from "@codemirror/lang-markdown";
+import { cpp } from "@codemirror/lang-cpp";
+import { java } from "@codemirror/lang-java";
 
 export default function Editor(props) {
+  let lang
+  if (props.lang === "javascript"){
+    lang = javascript({jsx : true})
+  }
+  if (props.lang === "python"){
+    lang = python({})
+  }
+  if (props.lang ==="html"){
+    lang=html({})
+  }
+  if (props.lang === "markdown"){
+    lang = markdown({})
+  }
+  if (props.lang === "cpp"){
+    lang = cpp({})
+  }
+  if (props.lang === "java"){
+    lang = java({})
+  }
   return (
     <CodeMirror
       value={props.value}
       height={props.lang === "javascript" ? "60vh" : "100%"}
-      extensions={[
-        props.lang === "javascript" ? javascript({ jsx: true }) : python({})
-      ]}
+      extensions={[lang]}
       onChange={(value, viewUpdate) => {
         props.handleOnChange(value);
       }}
